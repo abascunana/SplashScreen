43696d6510c4ac69f260d31ee9700963579a657a
abascunana <abascunanasanchez@cifpfbmoll.eu>
2022-12-19 20:11:33 +0100

    Animaciones

--- app/src/main/java/com/example/splashscreenandroid/MainActivity.java
+++ app/src/main/java/com/example/splashscreenandroid/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.splashscreenandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.dynamicanimation.animation.FlingAnimation;
 
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 ImageView[] mImageViews;
-Animation animation;
+TextView[] mTextViews;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +22,17 @@ Animation animation;
         ImageView imageView4 = (ImageView) findViewById(R.id.imageView13);
         ImageView imageView3 = (ImageView) findViewById(R.id.imageView12);
         mImageViews = new ImageView[]{imageView1,imageView2,imageView3,imageView4};
-     animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+        Animation  animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+        Animation  animation2 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.clockwise);
         for (int i = 0; i < mImageViews.length; i++) {
             mImageViews[i].setAnimation(animation);
         }
+        TextView textView1 = (TextView) findViewById(R.id.textView);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        mTextViews = new TextView[]{textView1,textView2};
+        for (int i = 0; i < mTextViews.length; i++) {
+            mTextViews[i].setAnimation(animation2);
+        }
+
     }
 }
